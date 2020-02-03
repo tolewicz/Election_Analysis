43804d2df70039b676b225dc72930c884fcdca5a
tolewicz <tomasz.olewicz@gmail.com>
2020-02-02 22:26:03 -0800

    updating all py files including homework

---
+++ PyPoll_Challenge.py
@@ -0,0 +1,118 @@
+#import key modules
+import csv
+import os
+
+
+# defining initial varaibles and empty objects
+total_votes = 0
+candidate_list = []
+candidate_votes = {}
+county_list = []
+county_poll = {}
+candidate_county_votes = {}
+winning_candidate = ""
+winning_votes = 0
+winning_percentage = 0
+winning_county = ""
+#"cwinning" means county winning 
+cwinning_votes = 0
+cwinning_percentage = 0
+
+
+
+# calling path to election results file
+csvpath_to_read = os.path.join('Resources','election_results.csv')
+
+#opening the file and assigning is as variable
+with open(csvpath_to_read) as file_to_read:
+    file_to_read = csv.reader(file_to_read)
+    headder = next(file_to_read) 
+
+
+#iterate over rows and get data from each row to count total votes
+    for row in file_to_read:
+        total_votes +=1
+
+        #identyfying candidate name and county name while reading each row
+        candidate_name = row[2]
+        county_name = row[1]
+
+        
+        #creating list of candidates (per challenge request)
+        #using dictionaries to calcualte the number of votes
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        #adding +1 vote to candidate name
+        candidate_votes[candidate_name] +=1
+
+        #creating list (per challenge request) and calculating total votes per county
+        if county_name not in county_list:
+            county_list.append(county_name)
+            county_poll[county_name] = 0
+        county_poll[county_name] += 1
+    print(county_poll)
+
+
+#opening a text file to save results
+file_to_save = os.path.join('Resources', 'election_analysis.txt')
+with open(file_to_save, "w") as txt_file:
+
+#printing # of total votes in terminal
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    
+    print(election_results)
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+#County turnout
+##counting number and percentage of votes
+    for county_name in county_poll:
+        cvotes = county_poll[county_name]
+        cvote_percentage = (float(cvotes)/float(total_votes))*100
+        county_results = (f"{county_name}: {cvote_percentage:.1f}% ({cvotes:,})\n")
+        print(county_results)
+        txt_file.write(county_results)
+        
+        ## determining the county with largest turnout
+        if (cvotes > cwinning_votes) and (cvote_percentage > cwinning_percentage):
+            cwinning_votes = cvotes
+            cwinning_percentage = cvote_percentage
+            winning_county = county_name
+
+    ## printing results
+    winning_county_summary = (f"----------------------------------------------- \n"
+                                f"Largest County Turnout: {winning_county} \n"
+                                f"----------------------------------------------- \n")
+    ##saving results to text file
+    print(winning_county_summary)
+    txt_file.write(winning_county_summary)
+
+# candidate winner
+## counting number and percentage of votes
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = (float(votes)/float(total_votes))*100
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
+        
+        ##determinig candidate with largest number and percentage of votes
+        if (votes > winning_votes) and (vote_percentage > winning_percentage):
+            winning_votes = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    ##printing results
+    winning_candidate_summary = (f"----------------------------------------------- \n"
+                                 f"Winner: {winning_candidate} \n"
+                                 f"Winning vote count: {winning_votes:,}\n"
+                                 f"Winning percentage: {winning_percentage:.1f}\n"
+                                 f"----------------------------------------------- \n")
+    ##saving results to text file
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Election_Analysis
-Module 3
+## Election_Analysis
+## Project Overview
+A Colorado Board Elections employee requested following task to complete the audit of a recent local congressional election:
+1.	Calculate the total vote count 
+2.	Determine the winner of the elections
+##Resources
+-	Data source: election_results.cvs
+-	Software: Python 3.7.6, Visual studio code
+## Summary
+The analysis of the elections shows that: 
+-	Total number of votes was 369,711
+-	There were 3 candidates in the elections:
+1.	Charles Casper Stockham
+2.	Diana DeGette
+3.	Raymon Anthony Doane
+The candidate results were as follows
+1.	Charles Casper Stockham received 23.0% of the vote and 85,213 number of votes
+2.	Diana DeGette received 73.8% of the vote and 272,892 number of votes
+3.	Raymon Anthony Doane: 3.1% of the vote and 11,606 number of votes
+The winner of the election was Diana DeGette with 73.8% of the vote and 272,892 number of votes
+## Challenge overview
+Expand the analysis to provide details about county vote ditribution
+1.	Determine the county with largest number of votes
+2.	Calculate total vote count and percentage per county
+3.	Calculate total vote count and percentage for each candidate
+## Challenge summary
+The analysis was conducted on 3 counties: Jefferson, Denver, Arapahoe
+The vote turnout per county was as follows:
+1.	Jefferson: had 10.5% votes turnout with 38,855 votes
+2.	Denver: had 82.8% votes turnout with 306,055 votes
+3.	Arapahoe: had 6.7% with 24,801 votes
+The county with larges turnout was Denver with 82.8% votes and 306,055 number of votes
+
